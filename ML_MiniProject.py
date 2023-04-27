6e46aa9dd8399c33e2c440d486743df1bdba1d80
Simran Desai <126150681+Sim-ranDesai@users.noreply.github.com>
2023-04-27 20:13:02 +0530

    Add files via upload

---
+++ ML_MiniProject.py
@@ -0,0 +1,39 @@
+
+
+'''
+                            Machine Learning Laboratory Mini Project : MALWARE DETECTION
+'''
+'''
+                                Contributers :
+                                UCE2021606 --> Sakshi Bajaj
+                                UCE2021616 --> Simran Desai
+                                UCE2021619 --> Sanika Deshpande
+                                UCE2021620 --> Gargee Dorle
+                
+'''
+
+
+
+
+print("*****************")
+print("MALWARE DETECTION")
+print("*****************")
+
+
+
+print("0. Exit")
+print("1. Random Forest")
+print("2. Logistic Regression")
+print("3. Neural Network")
+choice = int(input("Enter the algorithm that you choose to implement : "))
+
+
+
+# to exit the program alltogether 
+if choice == 0:
+    exit()
+
+
+# Implement the 
+if choice == 1:
+    
